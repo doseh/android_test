3a093f5bc3698354c8754d2ee5ceff45df9e670e
Ferran <feerraan@gmail.com>
2017-06-11 23:01:45 +0200

    nomes lo actualitzat

--- app/src/main/java/com/example/ferranramirez/mytest/MainActivity.java
+++ app/src/main/java/com/example/ferranramirez/mytest/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Comentari sense sentit
         setContentView(R.layout.activity_main);
     }
 }
